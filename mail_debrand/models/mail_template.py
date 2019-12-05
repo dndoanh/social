7151c07b545c87fe28cd898667e4e4bb89882218
Doanh Duong <dndoanh@gmail.com>
2019-12-05 14:45:25 +0700

    [FIX] fix mail debranding of Odoo logo

--- mail_debrand/models/mail_template.py
+++ mail_debrand/models/mail_template.py
@@ -2,7 +2,6 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from lxml import html as htmltree
-import re
 from odoo import _, api, models
 
 
@@ -11,29 +10,15 @@ class MailTemplate(models.Model):
 
     @api.model
     def _debrand_body(self, html):
-        using_word = _('using')
-        odoo_word = _('Odoo')
-        html = re.sub(
-            using_word + "(.*)[\r\n]*(.*)>" + odoo_word + r"</a>", "", html,
-        )
-        powered_by = _("Powered by")
-        if powered_by not in html:
-            return html
         root = htmltree.fromstring(html)
         powered_by_elements = root.xpath(
-            "//*[text()[contains(.,'%s')]]" % powered_by
+            "//table//a[contains(@href, 'www.odoo.com')]"
         )
         for elem in powered_by_elements:
-            # make sure it isn't a spurious powered by
-            if any(
-                [
-                    "www.odoo.com" in child.get("href", "")
-                    for child in elem.getchildren()
-                ]
-            ):
-                for child in elem.getchildren():
-                    elem.remove(child)
-                elem.text = None
+            parent = elem.getparent().getparent()
+            if parent is not None:
+                parent.drop_tree()
+
         return htmltree.tostring(root).decode("utf-8")
 
     @api.model
